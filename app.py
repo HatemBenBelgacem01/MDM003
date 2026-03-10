2862a16b019bafd2b99b7a4746a9a399b1fde117
Hatem Ben Belgacem <hatem.benbelgacem@outlook.com>
2026-03-10 15:27:19 +0100

    update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-def main():
-    print("Hello from mdm003!")
-
-
-if __name__ == "__main__":
-    main()
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+	return "<p>Hello, World!</p>"
