f76f9ef8c1469a3750ba2bddf7c5fe1442d6e7ae
Hatem Ben Belgacem <hatem.benbelgacem@outlook.com>
2026-03-11 15:20:53 +0100

    update

---
+++ web/script.js
@@ -0,0 +1,29 @@
+function getSentiment(event, text) {
+    if (!text || event.key !== "Enter") {
+        answer.innerHTML = '';
+        return;
+    }
+    answerPart.style.visibility = "visible";
+  
+    // Get Sentiment
+    fetch('/sentiment?' + new URLSearchParams({
+        text: text,
+    }), {
+        method: 'GET',
+        headers: {}
+    }).then(
+        response => {
+            console.log(response)
+            response.text().then(function (text) {
+                answer.innerHTML = text;
+            });
+
+            Text
+            zuweisen
+        }
+    ).then(
+        success => console.log(success)
+    ).catch(
+        error => console.log(error)
+    );
+}
